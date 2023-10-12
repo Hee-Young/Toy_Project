bcfd5243326f12fffe78a1e26ca6bd1512d49ab9
Hee-Young <66981758+Hee-Young@users.noreply.github.com>
2023-10-12 17:00:20 +0900

    Update 10000hours.js
    Add button click events

--- 10000hours/js/10000hours.js
+++ 10000hours/js/10000hours.js
@@ -1,1 +1,30 @@
-// js file
+const startButton = document.querySelector(".start_btn");
+const goButton = document.querySelector(".go");
+const shareButton = document.querySelector("share");
+const closeButton = document.querySelector(".close_btn");
+const modal = document.querySelector("#modal");
+
+function startCalculate() {
+    const fieldValue = document.querySelector("#field_value");
+    const timeValue = document.querySelector("#time_value");
+    let time = Number(timeValue.vlaue);
+
+    
+}
+
+function openModal() {
+    modal.style.display = "flex";
+}
+
+function closeModal() {
+    modal.style.display = "none";
+}
+
+function copyUrl() {
+    
+}
+
+startButton.addEventListener("click", startCalculate);
+goButton.addEventListener("click", startCalculate);
+shareButton.addEventListener("click", copyUrl);
+closeButton.addEventListener("click", closeModal);
