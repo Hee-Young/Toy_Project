804d960546d76182684d83c88d7ae664320e5d29
Hee-Young <66981758+Hee-Young@users.noreply.github.com>
2023-10-30 18:30:09 +0900

    Update app.js

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -3,13 +3,15 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.rect(50, 50, 100, 100);
-ctx.rect(150, 150, 100, 100);
-ctx.rect(250, 250, 100, 100);
+ctx.fillRect(210 - 40, 200 - 30, 15, 100);
+ctx.fillRect(350 - 40, 200 - 30, 15, 100);
+ctx.fillRect(260 - 40, 200 - 30, 60, 200);
+
+ctx.arc(250, 100, 50, 0, 2 * Math.PI);
 ctx.fill();
 
 ctx.beginPath();
-ctx.rect(350, 350, 100, 100);
-ctx.rect(450, 450, 100, 100);
-ctx.fillStyle = "red";
+ctx.fillStyle = "white";
+ctx.arc(260 + 10, 80, 8, Math.PI, 2 * Math.PI);
+ctx.arc(220 + 10, 80, 8, Math.PI, 2 * Math.PI);
 ctx.fill();
