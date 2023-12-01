a398187ef336a69bab5de54216aae674afe8673a
Hee-Young <kkhy991222@gmail.com>
2023-12-01 17:26:22 +0900

    웹 점수판 구현

---
+++ ScoreBoard/score.js
@@ -0,0 +1,23 @@
+const leftScore = document.querySelector("#scoreL");
+const rightScore = document.querySelector("#scoreR");
+const selectPlayerNum = document.querySelector("#numPlayer");
+const p1Button = document.querySelector("#player1");
+const p2Button = document.querySelector("#player2");
+const resetButton = document.querySelector("#scoreL");
+
+let p1Score = 0;
+let p2Score = 0;
+
+p1Button.addEventListener('click', funtion(){
+
+})
+p1Button.addEventListener('click', funtion(){
+
+})
+resetButton.addEventListener('click', function () {
+    p1Score = 0;
+    p2Score = 0;
+
+    leftScore.textContent = p1Score;
+
+})
